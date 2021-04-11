92b8b8d5028f58bc1f4ee1ca5b2d5e5fa44dcd3f
yu-krugliak <1-0_krugliak@ukr.net>
2021-04-11 23:24:05 +0300

    import fix

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
