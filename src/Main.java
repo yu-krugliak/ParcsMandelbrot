d22f82ccf089e06d4fbc2a0d9b49f406c98bf23c
yu-krugliak <1-0_krugliak@ukr.net>
2021-04-11 22:53:18 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,73 @@
+import parcs.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Start executing");
+        long startTime = System.nanoTime();
+
+        task curTask = new task();
+        curTask.addJarFile("MandelbrotAlgo.jar");
+
+        var xc = -0.74529;
+        var yc = 0.113075;
+        var zoom =  1.7E-4;
+        var maxIterations = 300;
+        var imageSize = 8192;
+
+        AMInfo info = new AMInfo(curTask, null);
+
+        System.out.println("Start executing");
+        long startTime = System.nanoTime();
+
+        List<channel> channels = new ArrayList<>();
+        var columnCount = 4;
+        var chunkSize = imageSize / columnCount;
+
+        for(int c = 0; c < columnCount; c++)
+        {
+            for (int r = 0; r < columnCount; r++)
+            {
+                point infoPoint = info.createPoint();
+                channel pointChannel = infoPoint.createChannel();
+                infoPoint.execute("MandelbrotAlgo");
+                pointChannel.write(xc);
+                pointChannel.write(yc);
+                pointChannel.write(zoom);
+                pointChannel.write(maxIterations);
+                pointChannel.write(imageSize);
+                pointChannel.write(c * chunkSize);//Chunk x pos
+                pointChannel.write(r * chunkSize);//Chunk y pos
+                pointChannel.write(chunkSize);
+                channels.add(pointChannel);
+            }
+        }
+
+        var fractalImage = new BufferedImage(imageSize, imageSize, BufferedImage.TYPE_INT_RGB);
+        var graphics = fractalImage.getGraphics();
+
+        for (parcs.channel channel : channels) {
+            //System.out.println("\n\n\n\n Processing article" + String.valueOf(i));
+            //System.out.println("Waiting for result...");
+
+            var xChunk = channel.readInt();
+            var yChunk = channel.readInt();
+            var fractalChunk = (BufferedImage) channel.readObject();
+            graphics.drawImage(fractalChunk, xChunk, yChunk, null);
+        }
+
+        File outputFile = new File("MandelbrotImage.jpg");
+        ImageIO.write(fractalImage, "jpg", outputFile);
+
+
+        double estimatedTime = (double) (System.nanoTime() - startTime) / 1000000000;
+        System.out.println("Time total (excluding IO): " + estimatedTime);
+
+        curTask.end();
+    }
+}
---
+++ src/MandelbrotAlgo.java
@@ -0,0 +1,66 @@
+import parcs.*;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class MandelbrotAlgo implements AM{
+    public void run(AMInfo info)
+    {
+        System.out.println("Start executing");
+        long startTime = System.nanoTime();
+
+        double xc = info.parent.readDouble();
+        double yc = info.parent.readDouble();
+        double zoom = info.parent.readDouble();
+        int maxIter = info.parent.readInt();
+        int imgSize = info.parent.readInt();
+        int xChunk = info.parent.readInt();
+        int yChunk = info.parent.readInt();
+        int chunkSize = info.parent.readInt();
+
+        var resultImg = GetMandelbrotChunk(xc, yc, zoom, maxIter, imgSize, xChunk, yChunk, chunkSize);
+
+        info.write(xChunk);
+        info.write(yChunk);
+        info.write(resultImg);
+
+        double estimatedTime = (double) (System.nanoTime() - startTime) / 1000000000;
+        System.out.println("Time total (excluding IO): " + estimatedTime);
+    }
+
+    public static BufferedImage GetMandelbrotChunk(double xc, double yc, double zoom, int maxIter, int imgSize, int xChunk, int yChunk, int chunkSize)
+    {
+        BufferedImage bufferedImage = new BufferedImage(chunkSize, chunkSize, BufferedImage.TYPE_INT_RGB);
+        double minX = xc - zoom / 2;
+        double minY = yc - zoom / 2;
+
+        Complex c0 = new Complex(0, 0);
+        Complex z0 = new Complex(0, 0);
+
+        for (int x = 0; x < chunkSize; x++) {
+            for (int y = 0; y < chunkSize; y++) {
+                c0.R = minX + ((xChunk + x) / (double)imgSize) * zoom;//Waiting for Krugliak
+                c0.I = minY + ((yChunk + y) / (double)imgSize) * zoom;
+                z0.R = 0;
+                z0.I = 0;
+
+                int iterations = 0;
+                do {
+                    iterations++;
+                    z0.Square();
+                    z0.Add(c0);
+                    if(z0.Magnitude() > 2.0) break;
+                }while (iterations < maxIter);
+
+                var colorFactor = Math.abs(1.0 - (double) iterations / (double) maxIter);
+                int gray = (int)(colorFactor * 255);
+                var colorColor = Color.getHSBColor((float) (colorFactor * 359) , 100, (int)(colorFactor * 100));
+                Color color = new Color(gray, gray, gray);
+
+                bufferedImage.setRGB(x, chunkSize - 1 - y, color.getRGB());
+            }
+        }
+        return bufferedImage;
+    }
+
+}
