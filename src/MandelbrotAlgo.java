adb3170acd181d2e5b30e927202eb68d4ea5f403
yu-krugliak <1-0_krugliak@ukr.net>
2021-04-12 23:20:05 +0300

    F

--- src/MandelbrotAlgo.java
+++ src/MandelbrotAlgo.java
@@ -1,11 +1,11 @@
 import parcs.*;
-
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
+//
+//import java.awt.*;
+//import java.awt.image.BufferedImage;
+//import java.io.ByteArrayOutputStream;
+//import java.io.IOException;
+//
+//import javax.imageio.ImageIO;
 
 public class MandelbrotAlgo implements AM{
     public void run(AMInfo info)
@@ -40,40 +40,40 @@ public class MandelbrotAlgo implements AM{
         double estimatedTime = (double) (System.nanoTime() - startTime) / 1000000000;
 //        System.out.println("Time total (excluding IO): " + estimatedTime);
     }
-
-    public static BufferedImage GetMandelbrotChunk(double xc, double yc, double zoom, int maxIter, int imgSize, int xChunk, int yChunk, int chunkSize)
-    {
-        BufferedImage bufferedImage = new BufferedImage(chunkSize, chunkSize, BufferedImage.TYPE_INT_RGB);
-        double minX = xc - zoom / 2;
-        double minY = yc - zoom / 2;
-
-        Complex c0 = new Complex(0, 0);
-        Complex z0 = new Complex(0, 0);
-
-        for (int x = 0; x < chunkSize; x++) {
-            for (int y = 0; y < chunkSize; y++) {
-                c0.R = minX + ((xChunk + x) / (double)imgSize) * zoom;
-                c0.I = minY + ((yChunk + y) / (double)imgSize) * zoom;
-                z0.R = 0;
-                z0.I = 0;
-
-                int iterations = 0;
-                do {
-                    iterations++;
-                    z0.Square();
-                    z0.Add(c0);
-                    if(z0.Magnitude() > 2.0) break;
-                }while (iterations < maxIter);
-
-                var colorFactor = Math.abs(1.0 - (double) iterations / (double) maxIter);
-                int gray = (int)(colorFactor * 255);
-                //var colorColor = Color.getHSBColor((float) (colorFactor * 359) , 100, (int)(colorFactor * 100));
-                Color color = new Color(gray, gray, gray);
-
-                bufferedImage.setRGB(x, chunkSize - 1 - y, color.getRGB());
-            }
-        }
-        return bufferedImage;
-    }
+//
+//    public static BufferedImage GetMandelbrotChunk(double xc, double yc, double zoom, int maxIter, int imgSize, int xChunk, int yChunk, int chunkSize)
+//    {
+//        BufferedImage bufferedImage = new BufferedImage(chunkSize, chunkSize, BufferedImage.TYPE_INT_RGB);
+//        double minX = xc - zoom / 2;
+//        double minY = yc - zoom / 2;
+//
+//        Complex c0 = new Complex(0, 0);
+//        Complex z0 = new Complex(0, 0);
+//
+//        for (int x = 0; x < chunkSize; x++) {
+//            for (int y = 0; y < chunkSize; y++) {
+//                c0.R = minX + ((xChunk + x) / (double)imgSize) * zoom;
+//                c0.I = minY + ((yChunk + y) / (double)imgSize) * zoom;
+//                z0.R = 0;
+//                z0.I = 0;
+//
+//                int iterations = 0;
+//                do {
+//                    iterations++;
+//                    z0.Square();
+//                    z0.Add(c0);
+//                    if(z0.Magnitude() > 2.0) break;
+//                }while (iterations < maxIter);
+//
+//                var colorFactor = Math.abs(1.0 - (double) iterations / (double) maxIter);
+//                int gray = (int)(colorFactor * 255);
+//                //var colorColor = Color.getHSBColor((float) (colorFactor * 359) , 100, (int)(colorFactor * 100));
+//                Color color = new Color(gray, gray, gray);
+//
+//                bufferedImage.setRGB(x, chunkSize - 1 - y, color.getRGB());
+//            }
+//        }
+//        return bufferedImage;
+//    }
 
 }
