c1c24036f82b5e1067427c29e6ea12a823b60ba7
yu-krugliak <1-0_krugliak@ukr.net>
2021-04-13 01:59:54 +0300

    Signed-off-by: yu-krugliak <1-0_krugliak@ukr.net>

--- UniParcs/src/Runner.java
+++ UniParcs/src/Runner.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Runner implements AM {
     @Override
     public void run(AMInfo info) {
-    	var test = info.parent.readObject();
+    	var test = (String)info.parent.readObject();
     	info.parent.write(test);
     }
 }
