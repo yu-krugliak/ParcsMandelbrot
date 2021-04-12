e76a37ecee7cbe9632149808c3b0d99b984fa253
yu-krugliak <1-0_krugliak@ukr.net>
2021-04-13 02:05:50 +0300

    ahha

--- src/Main.java
+++ src/Main.java
@@ -1,108 +1,48 @@
 import parcs.*;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main{
-public static void main(String[] argc) {
-    	try {
-    		task curtask = new task();
-    		curtask.addJarFile("MandelbrotAlgo.jar");
-    		var info = new AMInfo(curtask, (channel)null);
-    		
-            System.out.println("Start executing");
-            long startTime = System.nanoTime();
+public class Main {
+    public static void main(String[] args) throws Exception {
+        task curtask = new task();
+        curtask.addJarFile("Runner.jar");
+        AMInfo info = new AMInfo(curtask, null);
 
-            System.out.println("Point 1");
+        System.out.println("Loading image...");
+        File img = new File(curtask.findFile("big.png"));
+        BufferedImage newImage = ImageIO.read(img);
+        ImageMatrix imageMatrix = new ImageMatrix(newImage);
 
-            var xc = -0.74529;
-            var yc = 0.113075;
-            var zoom =  1.7E-4;
-            var maxIterations = 300;
-            var imageSize = 512;
+        List<List<Cell>> cells = imageMatrix.splitIntoCells();
 
-            List<channel> channels = new ArrayList<>();
-            var columnCount = 4;
-            int chunkSize = imageSize / columnCount;
-            System.out.println("Point 2");
-            /*
-            for(int c = 0; c < columnCount; c++)
-            {
-                for (int r = 0; r < columnCount; r++)
-                {
-                    point infoPoint = info.createPoint();
-                    channel pointChannel = infoPoint.createChannel();
-                    infoPoint.execute("MandelbrotAlgo");
-                    pointChannel.write(xc);
-                    pointChannel.write(yc);
-                    pointChannel.write(zoom);
-                    pointChannel.write(maxIterations);
-                    pointChannel.write(imageSize);
-                    pointChannel.write(c * chunkSize);//Chunk x pos
-                    pointChannel.write(r * chunkSize);//Chunk y pos
-                    pointChannel.write(chunkSize);
-                    channels.add(pointChannel);
-                }
-            }*/
-            point infoPoint = info.createPoint();
-            channel pointChannel = infoPoint.createChannel();
-            infoPoint.execute("MandelbrotAlgo");
-            pointChannel.write(xc);
-            pointChannel.write(yc);
-            pointChannel.write(zoom);
-            pointChannel.write(maxIterations);
-            pointChannel.write(imageSize);
-            pointChannel.write(100);//Chunk x pos
-            pointChannel.write(100);//Chunk y pos
-            pointChannel.write(chunkSize);
-            channels.add(pointChannel);
-            
-            System.out.println("Point 3");
-            var fractalImage = new BufferedImage(imageSize, imageSize, BufferedImage.TYPE_INT_RGB);
-            var graphics = fractalImage.getGraphics();
-            
-            System.out.println("Point 3.1");
-            for (parcs.channel channel : channels) {
-                //System.out.println("\n\n\n\n Processing article" + String.valueOf(i));
-                System.out.println("Waiting for result...");
-            	System.out.println("Point 3.2");
-            	
-            	System.out.println("Point 3.2.1");
-                var xChunk = channel.readInt();
-                System.out.println("Point 3.2.2");
-                var yChunk = channel.readInt();
-                System.out.println("Point 3.3");
-                //var bytesLength = channel.readInt();
-               // byte[] imgBytes = new byte[bytesLength];
-                System.out.println("Point 3.5");
-                //channel.read(imgBytes);
-                System.out.println("Point 3.6");
-                //InputStream is = new ByteArrayInputStream(imgBytes);
-                System.out.println("Point 3.7");
-                
-                //var fractalChunk = ImageIO.read(is);
-                //graphics.drawImage(fractalChunk, xChunk, yChunk, null);
-                
-                System.out.println("Processed chunk (" + xChunk + ", " + yChunk + ")");
-//            	var cx = channel.readDouble();
-//            	System.out.println(cx);
-            }
-            System.out.println("Point 4");
-            File outputFile = new File("MandelbrotImage.jpg");
-            ImageIO.write(fractalImage, "jpg", outputFile);
+        List<channel> channels = new ArrayList<>();
+        for (List<Cell> cellRow: cells) {
+            point p = info.createPoint();
+            channel c = p.createChannel();
+            p.execute("Runner");
+            c.write((Serializable) cellRow);
+            channels.add(c);
+        }
 
-            System.out.println("Point 5");
-            double estimatedTime = (double) (System.nanoTime() - startTime) / 1000000000;
-            System.out.println("Time total (excluding IO): " + estimatedTime);
-            System.out.println("Point 6");
-            
-            curtask.end();
-        }catch(IOException ex) {}
+        System.out.println("Waiting for result...");
+
+        List<List<Cell>> processedCells = new ArrayList<>();
+        for (parcs.channel channel : channels) {
+            List<Cell> cellRow = (List<Cell>) channel.readObject();
+            processedCells.add(cellRow);
+        }
+
+        System.out.println("Decoding image.");
+        BufferedImage processed = ImageMatrix.fromCells(processedCells).toBufferedImage();
+        File outputfile = new File("processedImage.jpg");
+        ImageIO.write(processed, "jpg", outputfile);
+        System.out.println("Done.");
+        curtask.end();
     }
 }
