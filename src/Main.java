cad125312ff0b79ab96e01b10f8cb9ededfd8c75
Влад Ильченко <prodigyvlad@gmail.com>
2021-04-13 04:01:53 +0300

    FINALLY FINALLY!!!!!!

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,7 @@ public static void main(String[] argc) {
             var yc = 0.113075;
             var zoom =  1.7E-4;
             var maxIterations = 300;
-            var imageSize = 2048;
+            var imageSize = 512;
 
             List<channel> channels = new ArrayList<>();
             var columnCount = 4;
